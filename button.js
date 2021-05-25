0d99f73f19012c603e73b418ca45c343a0014a46
Balasuryadeveloper <balasuryas44210@gmail.com>
2021-05-25 07:56:40 +0530

    javascript file was uploaded

---
+++ button.js
@@ -0,0 +1,23 @@
+function click(){
+    alert("Welcome to our Blood Bank System");
+}
+var a="Balasurya";
+function clickon1(){
+    document.getElementById("chennai").innerHTML=a;
+}
+var b="Abinesh";
+function clickon2(){
+    document.getElementById("coimbatore").innerHTML=b;
+}
+var c="Kowshik kumar";
+function clickon3(){
+    document.getElementById("salem").innerHTML=c;
+}
+var d="Balasurya";
+function clickon4(){
+    document.getElementById("namakkal").innerHTML=d;
+}
+var e="Abinesh";
+function clickon5(){
+    document.getElementById("erode").innerHTML=e;
+}
