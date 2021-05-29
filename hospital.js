b190293c25a40100fdcd2d98b047a1745100ead4
Balasuryadeveloper <balasuryas44210@gmail.com>
2021-05-29 12:43:15 +0530

    hospital.js file added

---
+++ hospital.js
@@ -0,0 +1,23 @@
+function hosLoad(hosDistrict,hosNamakkal,hosChennai,hosSalem,hosErode,hosCoimbatore){
+    hosDistrict=document.getElementById("hosDistrict").value;
+    hosNamakkal=document.getElementById("hosNamakkal").value;
+    hosChennai=document.getElementById("hosChennai").value;
+    hosSalem=document.getElementById("hosSalem").value;
+    hosErode=document.getElementById("hosErode").value;
+    hosCoimbatore=document.getElementById("hosCoimbatore").value;
+    if (hosDistrict == hosNamakkal) {
+        window.location="namakkal.html";
+    }
+    else if(hosDistrict == hosErode){
+        window.location="erode.html";
+    }
+    else if(hosDistrict == hosSalem){
+        window.location="salem.html";
+    }
+    else if(hosDistrict == hosChennai){
+        window.location="chennai.html";
+    }
+    else{
+        window.location="coimbatur.html";
+    }
+}
