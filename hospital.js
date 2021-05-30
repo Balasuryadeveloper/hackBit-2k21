1cfa092ccadcd423cbcb5cfd00846e28be5e1850
Balasuryadeveloper <balasuryas44210@gmail.com>
2021-05-30 09:03:58 +0530

    hospital details javascript completed

--- hospital.js
+++ hospital.js
@@ -1,23 +1,30 @@
-function hosLoad(hosDistrict,hosNamakkal,hosChennai,hosSalem,hosErode,hosCoimbatore){
+function hosLoad(hosDistrict,hosNamakkal,hosChennai,hosSalem,hosErode,hosCoimbatore,uname1){
     hosDistrict=document.getElementById("hosDistrict").value;
     hosNamakkal=document.getElementById("hosNamakkal").value;
     hosChennai=document.getElementById("hosChennai").value;
     hosSalem=document.getElementById("hosSalem").value;
     hosErode=document.getElementById("hosErode").value;
     hosCoimbatore=document.getElementById("hosCoimbatore").value;
-    if (hosDistrict == hosNamakkal) {
-        window.location="namakkal.html";
-    }
-    else if(hosDistrict == hosErode){
-        window.location="erode.html";
-    }
-    else if(hosDistrict == hosSalem){
-        window.location="salem.html";
-    }
-    else if(hosDistrict == hosChennai){
-        window.location="chennai.html";
+    uname1=String(document.getElementById("uname1").value);
+    pnumber1=String(document.getElementById("pnumber1").value);
+    if (uname1 != 0 && pnumber1 != 0) {
+        if (hosDistrict == hosNamakkal) {
+            window.location="namakkal.html";
+        }
+        else if(hosDistrict == hosErode){
+            window.location="erode.html";
+        }
+        else if(hosDistrict == hosSalem){
+            window.location="salem.html";
+        }
+        else if(hosDistrict == hosChennai){
+            window.location="chennai.html";
+        }
+        else{
+            window.location="coimbatur.html";
+        }
     }
     else{
-        window.location="coimbatur.html";
+        alert("Please fill the form correctly");
     }
 }
