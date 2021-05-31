4c6f18a8b76f5ebb3110c2f8c2e96b8dbaca30e2
Balasuryadeveloper <balasuryas44210@gmail.com>
2021-05-31 09:22:29 +0530

    blood group seperated

--- bloodbank.js
+++ bloodbank.js
@@ -1,4 +1,4 @@
-function load(district,namakkal,chennai,salem,erode,coimbatore,name,pn){
+function load(district,namakkal,chennai,salem,erode,coimbatore,name,pn,bg,ap,an,bp,bn,op,on,abp,abn){
     district=String(document.getElementById("district").value);
     chennai=String(document.getElementById("chennai").value);
     namakkal=String(document.getElementById("namakkal").value);
@@ -7,21 +7,39 @@ function load(district,namakkal,chennai,salem,erode,coimbatore,name,pn){
     coimbatore=String(document.getElementById("chennai").value);
     name=String(document.getElementById("name").value);
     pn=Number(document.getElementById("pn").value);
-    if (name != "" && pn != 0) {
-        if (district == chennai) {
-            window.location="chennaiblood.html";
+    bg=document.getElementById("bloodgroup").value;
+    ap=document.getElementById("ap").value;
+    an=document.getElementById("an").value;
+    bp=document.getElementById("bp").value;
+    bn=document.getElementById("bn").value;
+    op=document.getElementById("op").value;
+    on=document.getElementById("on").value;
+    abp=document.getElementById("abp").value;
+    abn=document.getElementById("abn").value;
+    if (name != "" && pn != 0 && bg != 0) {
+        if (district == chennai && bg == ap) {
+            window.location="apchennai.html";
         }
-        else if (district == namakkal) {
-            window.location="namakkalblood.html";
+        else if (district == chennai && bg == an) {
+            window.location="anchennai.html";
         }
-        else if (district == salem){
-            window.location="salemblood.html";
+        else if (district == chennai && bg == bp) {
+            window.location="bpchennai.html";
         }
-        else if (district == erode) {
-            window.location="erodeblood.html";
+        else if (district == chennai && bg == bn) {
+            window.location="anchennai.html";
         }
-        else {
-            window.location="coimbatoreblood.html";
+        else if (district == chennai && bg == op) {
+            window.location="opchennai.html";
+        }
+        else if (district == chennai && bg == on) {
+            window.location="onchennai.html";
+        }
+        else if (district == chennai && bg == abp) {
+            window.location="abpchennai.html";
+        }
+        else if (district == chennai && bg == abn) {
+            window.location="abnchennai.html";
         }
     }
     else{
